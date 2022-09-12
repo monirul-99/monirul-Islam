d59868d6ca10dfa8c7988a31f154a83c38bee66c
monaulic <sohelmuhammad164@gmail.com>
2022-09-12 12:13:45 +0600

    image added for cover

---
+++ setting.js
@@ -0,0 +1,12 @@
+// const inputValueById = (id) => {
+//     const inputText = document.getElementById(id).value;
+//     return inputText;
+// }
+
+// document.getElementById('stylishBtn').addEventListener('click', function(){
+//     const input = inputValueById('nameInput');
+//     console.log(input)
+// })
+
+const m =[12, 34, 56].filter(p=>p > 100)
+console.log(m)
